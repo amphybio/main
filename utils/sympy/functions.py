9a2ef321d98d26c132b5a16b8dd63fe43b587d78
Leonardo Gama <leonardo.reis.gama@gmail.com>
2019-06-24 11:53:02 -0300

    sympy: latex pretty printers

--- utils/sympy/functions.py
+++ utils/sympy/functions.py
@@ -21,6 +21,9 @@ class KummerM(hyper):
     def _pretty(self, printer):
         p = printer.doprint
         return prettyForm("M({}, {}, {})".format(p(self.args[0][0]), p(self.args[1][0]), p(self.args[2])))
+    def _latex(self, printer):
+        p = printer.doprint
+        return r"\mathcal{{M}}\left({},\ {},\ {}\right)".format(p(self.args[0][0]), p(self.args[1][0]), p(self.args[2]))
 
 # Temporary monkey patch:
 KummerM = lambda a, b, z: hyper((a,), (b,), z)
@@ -34,8 +37,11 @@ class pochhammer(Function):
     """
     @classmethod
     def eval(cls, x, n):
-        if n.is_real:
+        if n.is_integer:
             return prod((x + i for i in range(n)))
     def _pretty(self, printer):
         p = printer.doprint
         return prettyForm("({}){}".format(p(self.args[0]), subscript(p(self.args[1]))))
+    def _latex(self, printer):
+        p = printer.doprint
+        return r"\left({}\right)_{{{}}}".format(p(self.args[0]), p(self.args[1]))
