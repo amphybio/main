61e7707992f39976083d5e0677a87183b41a313e
Leonardo Gama <leonardo.reis.gama@gmail.com>
2019-10-09 22:03:54 -0300

    python utils: almost functional memoize decorator

--- utils/__init__.py
+++ utils/__init__.py
@@ -20,26 +20,36 @@ def _normalize_type(obj):
     if iscontainer(obj):
         return tuple(_normalize_type(o) for o in obj)
     elif isnumeric(obj):
-        return float(obj)
+        return (float(obj),)
     else
         return obj
 
+HASH_MASK = 2**16 - 1
 def memoize(func, *, loc=CACHE_DIR, match_type=True, ignore=None):
     """Persistent memoization function decorators.
 
     :returns: TODO
 
     """
-    func.id = func.__name__
+    func.id = "{}.{:04x}".format(func.__qualname__, hash(func.__code__.co_code) & HASH_MASK)
     func.cache_path = os.path.join(loc, func_id)
+    func.match_type = match_type
+    #TODO: need to set as attribute? or wrapped functions can access local variables of wrapper?
+    func.args = inspect.getfullargspec(func).args
+    if ignore is not None:
+        try:
+            ignore = set(ignore)
+        except:
+            ignore = {ignore}
 
     def wrapper(*args, **kwargs):
-        args, kwargs = inspect.getfullargspec(func, *args, **kwargs) 
-        #ignore...
-        if not match_type:
+        if ignore is not None:
+            args = [args[func.args.index(a)] for a in func.args if a not in ignore]
+            kwargs = {k: v for k, v in kwargs.items() if k not in ignore}
+        if not func.match_type:
             args = _normalize_type(args)
             kwargs = {k: _normalize_type(v) for k, v in kwargs.items()}
-        key = str(args) + str(kwargs)
+        key = repr(args) + repr(sorted((k, v) for k, v in kwargs.items()))
 
         with shelve.open(func.cache_path) as db:
             try:
