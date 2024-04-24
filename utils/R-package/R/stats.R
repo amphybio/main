e8d393eab03ace8ad76a6a801c9aca42dba8c2e7
Leonardo Gama <leonardo.gama@usp.br>
2024-04-24 12:18:12 -0300

    update copyright notice

--- utils/R-package/R/stats.R
+++ utils/R-package/R/stats.R
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, you can obtain one at https://mozilla.org/MPL/2.0/
 #
-# Copyright 2023 Alexandre Ferreira Ramos - AMPhyBio Laboratory
+# Copyright 2023-2024 Alexandre Ramos, AMPhyBio Laboratory <alex.ramos@usp.br>
 #
 # Project:  AMPhyBio Utilities
 # Version:  0.1
