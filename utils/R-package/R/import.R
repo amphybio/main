5ce77d39cf5f17d46f4f8fbed116386fb2687dca
Leonardo Gama <leonardo.gama@usp.br>
2024-05-09 12:33:58 -0300

    Additions to the "amphybio" R package of utilities. (#5)
    Public functions added to the package by file:
    
    hashing.R
        - hash(): calculate a 32-bit hash using the Fowler-Noll-Vo hash function
    import.R
        - sheet_cols(): renamed from excel_cols()
        - as_ordered_keep_levels() convert a factor to an ordered (factor) without dropping unused levels
        - read_sheet(): read a single sheet from a spreadsheet file in Microsoft Excel's format applying some consistency checks
        - construct_table(): construct a tidy (well formatted) data.frame based on manual data annotation, consolidating data from one or more tabulated files
    processing.R
        - combat(): convenience function to apply the ComBat method of batch effect correction for data frames
    
    ---
    
    Squashed commits:
    
    * update copyright notice
    * R utils: first version of read_sheet() function
    * first version of extract and format functions
    * finished first version of read_sheet()
    * ordered and guess_max fixes in progress
    * added FNV hash function to utilities
    * use FNV hash function in read_sheet()
    * finished functions to extract formatted tables from raw data
    * rename "as_ordered" function to "ordered"
    * moved hash function to separate file
    * R-utils: reorganization
    * R utils: added convenience wrapper function for ComBat (not tested yet)
    * R-utils: combat() fixes
    * R-utils: add sva to Suggests

--- utils/R-package/R/import.R
+++ utils/R-package/R/import.R
@@ -2,17 +2,17 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, you can obtain one at https://mozilla.org/MPL/2.0/
 #
-# Copyright 2023 Alexandre Ferreira Ramos - AMPhyBio Laboratory
+# Copyright 2023-2024 Alexandre Ramos, AMPhyBio Laboratory <alex.ramos@usp.br>
 #
-# Project:  AMPhyBio Code Library
+# Project:  AMPhyBio Utilities Library
 # Version:  0.1
 # Created:  06-12-2020
 # Authors:
 #   - Leonardo Gama <leonardo.gama@usp.br> <leogama@github>
 
 
-EXCEL_COLS <- expand.grid(LETTERS, c('', LETTERS))
-EXCEL_COLS <- setNames(1:(26*27), paste0(EXCEL_COLS[[2]], EXCEL_COLS[[1]]))
+HEADER_NAME <- expand.grid(LETTERS, c('', LETTERS))
+HEADER_NAME <- 1:(26*27) |> set_names(str_c(HEADER_NAME[[2]], HEADER_NAME[[1]]))
 
 #' Convert spreadsheet column ranges to numeric indexes
 #'
@@ -24,26 +24,371 @@ EXCEL_COLS <- setNames(1:(26*27), paste0(EXCEL_COLS[[2]], EXCEL_COLS[[1]]))
 #'
 #' @export
 
-excel_cols <- function(...) {
+sheet_cols <- function(...) {
     args <- toupper(as.character(list(...)))
     if (length(args) == 1) {
         # Accept indexes as a comma separated list in a character string.
         args <- strsplit(trimws(args), '[[:space:]]*,[[:space:]]*')[[1]]
     }
-    if (length(invalid <- grep('^[A-Z]{1,2}(:[A-Z]{1,2})?$', args, invert=TRUE, value=TRUE))) {
-        stop("Invalid column index or range:  ", paste(invalid, collapse=", "))
+    invalid <- grep('^[A-Z]{1,2}(:[A-Z]{1,2})?$', args, invert = TRUE, value = TRUE)
+    if (length(invalid) > 0) {
+        stop("Invalid column index or range:  ", paste(invalid, collapse = ", "))
     }
 
     cols <- integer()
     for (col_index in strsplit(args, ':')) {
         if (length(col_index) == 1) {
-            cols <- c(cols, EXCEL_COLS[col_index[[1]]])
+            cols <- c(cols, HEADER_NAME[col_index[[1]]])
         } else {
             # Preserve columns' alphabetical indexes in names.
-            range_begin <- EXCEL_COLS[col_index[[1]]]
-            range_end <- EXCEL_COLS[col_index[[2]]]
-            cols <- c(cols, EXCEL_COLS[range_begin:range_end])
+            range_begin <- HEADER_NAME[col_index[[1]]]
+            range_end <- HEADER_NAME[col_index[[2]]]
+            cols <- c(cols, HEADER_NAME[range_begin:range_end])
         }
     }
     cols
+}
+
+
+#' Convert factor to ordered keeping unused levels.
+#'
+#' By default, ordered() and as.ordered() drop unused levels.
+#'
+#' @param x  A factor
+#' @returns  An ordered factor
+#' @seealso [as.ordered()]
+#' @export
+
+as_ordered_keep_levels <- function(x) {
+    if (is.ordered(x)) {
+        x
+    } else if (!is.factor(x)) {
+        ordered(x)
+    } else {
+        factor(x, levels = levels(x), ordered = TRUE)
+    }
+}
+
+
+default_casting <- list(
+    binary = as.logical,
+    nominal = as.factor,
+    ordinal = as_ordered_keep_levels,
+    discrete = as.integer,
+    continuous = as.numeric,
+    date = as.Date,  # use POSIX time?
+    text = as.character
+)
+
+
+read_sheet <- function(
+        path,
+        sheet = NULL,
+        range = NULL,
+        guess_max = 1000,
+        check = TRUE,
+        checksum = NULL,
+        checksum_func = tools::md5sum,
+        rows = NULL,
+        id_col = NULL,
+        id_cast = NULL,
+        sensible_cols = NULL,
+        hash_func = hash) {
+
+    #'  Read an Excel sheet applying some checks.
+    #'
+    #'  Read a single sheet from a spreadsheet file in Microsoft Excel's format
+    #'  into a data.frame using the readxl package.  Check the file existence,
+    #'  its checksum, the number of rows expected to be read and...
+    #'
+    #'  Parameters:
+    #'      path (character): Path to the xls/xlsx file.
+    #'      sheet (character|integer):
+    #'          Sheet to read.  Either a string (the name of a sheet), or an
+    #'          integer (the position of the sheet).  Defaults to the first
+    #'          sheet in the file
+    #'      range (character):
+    #'          A cell range to read from, as described in cell-specification.
+    #'          Includes typical Excel ranges like "B3:D87" and more.
+    #'      guess_max (integer):
+    #'          Maximum number of data rows to use for guessing column types.
+    #'      check (logical): Whether to apply the checks (see below).
+    #'      checksum (character): Checksum string of the file in 'path'.
+    #'      rows (integer):
+    #'          Expected number of rows in the returned table. If 'id_col' is
+    #'          specified, this is the number of rows left after dropping those
+    #'          with NA values in the identifier column.
+    #'      id_col (character|integer):
+    #'          If an integer, the number of the column to be used as
+    #'          identifier.  If text, a regular expression that matches the name
+    #'          of that column.
+    #'      id_cast (function):
+    #'          A function applied to the values of the identifier column.
+    #'          Example: 'as.integer'.
+    #'      sensible_cols (integer|character):
+    #'          Numbers or names of columns that contain sensible data and
+    #'          should be annonymized by hashing its values with 'hash_func'.
+    #'      hash_func (function):
+    #'          A function that converts vectors (the columns specifiend by
+    #'          'sensible_cols') to vectors of hash strings.
+    #'
+    #'  Returns:
+    #'      data.frame: The read table (sheet of spreadsheet file).
+    #'
+    #'  Raises:
+    #'      Always stops if:
+    #'          - the expression in 'id_col' doesn't match exactly 1 column
+    #'      When 'check' is TRUE, stops if:
+    #'          - the file pointed by 'path' is not accessible for any reason
+    #'          - the 'checksum' value doesn't match the file's checksum
+    #'          - the number of rows with valid identifiers isn't equal 'rows'
+    #'
+    #'  See Also:
+    #'      `vignette('sheet-geometry', package = 'readxl')`
+
+    #TODO: write simple examples
+    #  Example:
+    #
+    #      sheets <- readxl::read_excel('data/annotation.xlsx, sheet = 'sheets') |>
+    #          column_to_rownames('name')  # also casts to data.frame
+    #
+    #      raw_dat <- list()
+    #
+    #      for (i in seq(nrow(sheets))) {
+    #          name <- rownames(sheets)[i]
+    #          raw_dat[[name]] <- with(sheets[i, ], {
+    #              read_sheet(
+    #                  path = user::data_file(filename),
+    #                  sheet = sheet_name,
+    #                  range = range,
+    #                  guess_max = 10000,
+    #                  checksum = md5sum,
+    #                  id_col = id_col,
+    #                  id_cast = as.integer,
+    #                  sensible_cols = if (!is.na(sensible_cols)) str_split_1(sensible_cols, ';'),
+    #                  rows = rows
+    #              )
+    #          })
+    #      }
+    #
+    #  # Reexport data to tabulated files.
+    #  for (name in names(raw_dat)) {
+    #      filename <- sprintf('data/%s.tsv', name)
+    #      write.table(raw_dat[[name]], filename, sep = '\t', row.names = FALSE)
+    #  }
+
+    is_nil <- function(x) is.null(x) || (length(x) == 1 && is.na(x))
+
+    # Verify data file exists and is readable.
+    if (check && file.access(path, mode = 4) != 0) {
+        stop(sprintf("file is not accessible: '%s'", path))
+    }
+
+    # Check file integrity with checksum.
+    if (check && !is_nil(checksum)
+            && !identical(checksum, unname(checksum_func(path)))) {
+        stop(sprintf("does not match checksum: '%s'", path))
+    }
+
+    # Read single sheet.
+    cat(sprintf("Reading '%s'\n", path))
+
+    #if (missing(guess_max) && !missing(range) && is.character(range)) {
+        #TODO
+    #}
+
+    dat <- path |>
+        readxl::read_excel(if (!is_nil(sheet)) sheet, range, guess_max = guess_max) |>
+        #FIXME: trim_ws from read_excel is ignored (using str_trim)
+        mutate(across(where(is.character), str_trim)) |>
+        as.data.frame()
+
+    # Remove spurious whitespaces from column headers.
+    colnames(dat) <- colnames(dat) |> str_squish()
+
+    # Manipulate the ID column.
+    if (!is_nil(id_col)) {
+        # Match exact regex to find the ID column.
+        if (is.character(id_col)) {
+            match_col <- colnames(dat) |>
+                str_which(str_c('^', id_col, '$'))
+
+            #TODO: cover all cases
+            if (length(match_col) != 1) {
+                "the 'id_col' expression matched %d columns: '%s'" |>
+                    sprintf(length(match_col), id_col) |>
+                    stop()
+            }
+
+            id_col <- match_col
+        }
+
+        # Relocate and rename the ID column to 'id'.
+        dat <- dat |> relocate(id = id_col, .before = 1)
+
+        # Cast id column and drop invalid/empty entries.
+        if (!is.null(id_cast)) {
+            dat[['id']] <- suppressWarnings(id_cast(dat[['id']]))
+        }
+        dat <- dat |> filter(!is.na(id))
+    }
+
+    # Calculate hash of sensible (unused) data.
+    if (!is_nil(sensible_cols)) for (col in sensible_cols) {
+        dat[[col]] <- hash_func(dat[[col]])
+    }
+
+    # Check if the number of rows is the expected.
+    if (check && !is_nil(rows)) {
+        stopifnot(nrow(dat) == rows)
+    }
+
+    dat
+}
+
+
+## Functions for data extraction, formatting and validation ##
+
+annotation <- purrr::attr_getter('annotation')
+
+construct_table <- function(
+        table_name,
+        origin_tables,
+        annot_table,
+        check = TRUE) {
+
+    #'  Generate a tidy (well format) data.frame from one or potentially more
+    #'  tabulated file's original data.
+    #'
+    #'  Long description...
+    #'
+    #'  Parameters:
+    #'      table_name (character):
+    #'          The tables's identification in the column named "table" in the
+    #'          'annot_table' data.frame.
+    #'      origin_tables (list[data.frame]):
+    #'          A list containing the data.frames corresponding to each
+    #'          tabulated file or sheet of original data.
+    #'      annot_table (data.frame): annotation of variables.
+    #'      check (logical): whether to perform checks on data.
+    #'
+    #'  Returns:
+    #'      data.frame: The read table (sheet of spreadsheet file).
+    #'
+    #'  Raises:
+    #'      Always stops if:
+    #'          - the expression in 'id_col' doesn't match exactly 1 column
+    #'      When 'check' is TRUE, stops if:
+    #'          - the file pointed by 'path' is not accessible for any reason
+    #'          - the 'checksum' value doesn't match the file's checksum
+    #'          - the number of rows with valid identifiers isn't equal 'rows'
+    #'
+    #'  See Also:
+    #'      `vignette('sheet-geometry', package = 'readxl')`
+
+    data <- table_name |>
+        construct_from_annotation(origin_tables, annot_table) |>
+        relabel_factors() |>
+        cast_types() |>
+        generate_derived()
+
+    #TODO: implement and test
+    #if (check) {
+    #    check_types_values(data)
+    #}
+
+    data
+}
+
+
+construct_from_annotation <- function(
+        table_name,
+        origin_tables,
+        annot_table,
+        col_types = NULL) {
+
+    # Get primary columns for this table.
+    primary_columns <- annot_table |>
+        filter(table == table_name, origin != 'ALL', origin != 'DERIVED') |>
+        mutate(header = coalesce(header, variable))
+
+    #with(primary_columns, print(variable |> set_names(header)))
+
+    # Extract the specified columns from the original tables.
+    parts <- list()
+    for (origin_table in unique(primary_columns$origin)) {
+        columns <- primary_columns |>
+            filter(origin == origin_table) |>
+            pull(variable, header)
+        parts[[origin_table]] <- origin_tables[[origin_table]] |>
+            select(id, all_of(names(columns))) |>
+            rename_with(purrr::partial(extract, columns), !id)
+    }
+
+    # Generate the 'annotation' attribute.
+    annot_table <- annot_table |>
+        filter(table == table_name) |>
+        select(!table) |>
+        column_to_rownames('variable')
+
+    # Join parts and bind annotation.
+    parts |>
+        purrr::reduce(full_join, by = 'id') |>
+        set_attr('annotation', annot_table) %>%
+        set_class(c('annot_df', class(.)))
+}
+
+
+relabel_factors <- function(data) {
+
+    relabel_columns <- annotation(data) |>
+        filter(!is.na(levels))
+
+    for (column in rownames(relabel_columns) |> intersect(colnames(data))) {
+
+        levels <- relabel_columns[column, 'valid'] |> str_split_1(';')
+        labels <- relabel_columns[column, 'levels'] |> str_split_1(';')
+        data[[column]] <- data[[column]] |> factor(levels, labels)
+    }
+
+    data
+}
+
+
+cast_types <- function(data, casting = NULL) {
+
+    cast_func <- default_casting
+    if (!is.null(casting))
+        cast_func[names(casting)] <- casting
+
+    cast_columns <- annotation(data) |>
+        filter(origin != 'ALL', origin != 'DERIVED') %>%
+        { pull(., class) |> set_names(rownames(.)) }
+
+    for (name in names(cast_columns)) {
+        cast <- cast_func[[cast_columns[name]]]
+        data[[name]] <- cast(data[[name]])
+    }
+
+    data
+}
+
+
+generate_derived <- function(data) {
+
+    derived_columns <- annotation(data) |>
+        filter(origin == 'DERIVED') %>%
+        { pull(., transform) |> set_names(rownames(.)) }
+
+    for (name in names(derived_columns))
+        data[[name]] <- data |>
+            with(derived_columns[name] |> rlang::parse_expr() |> eval())
+    data
+}
+
+
+check_types_values <- function(data) {
+
+    validators <- annotation(data) |>
+        filter(!is.na(valid))
 }
---
+++ utils/R-package/R/processing.R
@@ -0,0 +1,101 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at https://mozilla.org/MPL/2.0/
+#
+# Copyright 2023-2024 Alexandre Ramos, AMPhyBio Laboratory <alex.ramos@usp.br>
+#
+# Project:  AMPhyBio Utilities Library
+# Version:  0.1
+# Created:  08-04-2024
+# Authors:
+#   - Leonardo Gama <leonardo.gama@usp.br> <leogama@github>
+
+
+combat <- function(
+        data,
+        batch,
+        vars = NULL,
+        covars = NULL,
+        parametric = TRUE,
+        mean.only = FALSE,
+        ref.batch = NULL,
+        ...) {
+
+    #'  Apply the ComBat batch effect correction method to tabular data.
+    #'
+    #'  Convenience function to apply `sva::ComBat()` to data in the
+    #'  common `data.frame` format and with less clunky parameters.
+    #'
+    #'  Parameters:
+    #'      data (data.frame): Containing variables to be corrected.
+    #'      batch (character(1)|factor):
+    #'          Either the name of the column with batch information or
+    #'          a vector the same length as the number of rows in 'data'
+    #'          containing this information (categorical).
+    #'      vars (character):
+    #'          Names of the columns to correct for batch effect. If
+    #'          `NULL`, the default, all numeric columns except those
+    #'          listed in 'batch' and 'covars' are transformed.
+    #'      covars (formula):
+    #'          Formula with the outcome of interest variable and other
+    #'          possible covariates besides batch. Example:
+    #'          `~ outcome + covar1 + covar2`
+    #'      parametrict (logical(1)):
+    #'          If `TRUE` (default) use the parametric version of the
+    #'          ComBat method, which assumes normality of data.
+    #'      mean.only (logical(1):
+    #'          If TRUE ComBat only corrects the mean of the batch
+    #'          effect (no scale adjustment)
+    #'      ref.batch: (character(1)):
+    #'          If given, will use the selected batch as a reference for
+    #'          batch adjustment.
+    #'      ...: Extra arguments passed to `sva::ComBat()`.
+    #'
+    #'  Returns:
+    #'      data.frame: the data corrected for batch effect.
+    #'
+    #'  See also:
+    #'      `sva::ComBat()`
+    #'
+    #'  @export
+
+    if (!requireNamespace('sva', quietly = TRUE)) {
+        stop("Package 'sva' is not installed but is required.")
+    }
+
+    if (missing(vars)) {
+        num_data <- data |> select(where(is.numeric), -all_of(covars)) 
+        if (length(batch) == 1) {
+            num_data <- num_data |> select(-any_of(batch))
+        }
+    } else {
+        num_data <- data |> select(all_of(vars))
+    }
+
+    if (length(batch) == 1) {
+        batch <- data[[batch]]
+    }
+
+    mod <- if (!is.null(covars)) {
+        str_c('~', str_c(covars, collapse = '+')) |>
+            formula() |>
+            model.matrix(data)
+    }
+
+    # NOTE: The original function was developed for gene expression data
+    # and expects the input in a transposed form (variables in rows and
+    # samples in columns).
+    num_data <- t(num_data)
+    res <- suppressMessages(sva::ComBat(
+        num_data,
+        batch = batch,
+        mod = mod,
+        par.prior = parametric,
+        mean.only = mean.only,
+        ref.batch = ref.batch,
+        ...)) |>
+    t() |>
+    as.data.frame()
+
+    data |> mutate(res)
+}
